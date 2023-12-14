b8a2084e12377aed0fdcae325152fcae6d39d3a3
Evanise C Silva <evanisecs@gmail.com>
2023-12-14 20:08:23 -0300

    devConverter

---
+++ devConverter/script.js
@@ -0,0 +1,10 @@
+const form = document.getElementById('form');
+form.addEventListener('submit, handlesubmit')
+
+const inputvalue = document.getElementById('value-real');
+const selectedCurrency = document.getElementById('currency');
+const result = document.getElementById('result');
+
+function handlesubmit(e) {
+  alert('ALOOO')
+};
